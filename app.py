3b2520c7104c83d508d0bd77c0b3c23da55fce2d
SamueleCreti <samu.2006.br@gmail.com>
2024-11-26 11:33:26 +0000

    Commit 1

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def welcome():
+    return render_template("welcome.html")
 
-app.run()
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
