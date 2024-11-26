4ab573c54e426a85764f00f2475881d6f15c590b
SamueleCreti <samu.2006.br@gmail.com>
2024-11-26 11:38:14 +0000

    Commit 2

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return render_template("welcome.html")
+    return render_template("welcome.html", titolo="Welcome")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", titolo="About")
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", titolo="Contact")
 
 
 if __name__ == '__main__':
